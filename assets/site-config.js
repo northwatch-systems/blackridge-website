3eb7db16370d59ee571e07493b6c20f2585c88cb
Daniel Kendrick <shellfu@users.noreply.github.com>
2026-06-21 13:52:27 -0600

    changes

---
+++ assets/site-config.js
@@ -0,0 +1,30 @@
+var SITE_CONFIG = {
+  siteName: "Northwatch Systems",
+  productName: "Blackridge",
+  category: "AI Spend Forensics",
+  canonicalDomain: "https://northwatch.systems",
+  contactEmail: "sales@northwatch.systems",
+  adminEmail: "admin@northwatch.systems",
+  lastUpdated: "June 2026",
+
+  // TODO: Add approved company LinkedIn URL
+  companyLinkedIn: "",
+
+  // TODO: Add approved founder LinkedIn profile URLs
+  founderLinkedIn: [],
+
+  // TODO: Add approved GitHub organization URL
+  github: "",
+
+  // TODO: Add approved X/Twitter URL
+  xTwitter: "",
+
+  // TODO: Add approved product demo URL if public
+  productDemo: "",
+
+  // TODO: Add approved technical blog URL
+  technicalBlog: "",
+
+  // TODO: Add approved documentation URL
+  docs: ""
+};
